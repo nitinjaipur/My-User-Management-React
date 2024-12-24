75fb560d77c9e960cf6c999e588cb4fd97f607b0
Nitin Mishra <nitin@amplework.com>
2024-12-24 16:16:33 +0530

    cicd test

--- src/modules/auth/userAuth/SignIn.jsx
+++ src/modules/auth/userAuth/SignIn.jsx
@@ -21,6 +21,7 @@ const SignIn = () => {
             <div className="bg-backgroundGray flex flex-col w-1/2 border-2 rounded-lg p-4 gap-4 items-center overflow-auto">
                 <p>{t('signInForm')}</p>
                 <p>{t('welcome')}</p>
+                <p>test</p>
                 <div className="flex flex-col w-full gap-4 items-center">
                     <InputBox label={t('email')} type="email" name={'email'} value={state.email} onValueChange={onStateChange}/>
                     <InputBox label={t('password')} type='password' name={'password'} value={state.password} onValueChange={onStateChange}/>
