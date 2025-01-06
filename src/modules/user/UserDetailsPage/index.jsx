c6e13285013802cb980058283ef2d71cdc0bff4e
Nitin Mishra <nitin@amplework.com>
2025-01-06 15:26:59 +0530

    integrated redux-persist and remove stict mode

--- src/modules/user/UserDetailsPage/index.jsx
+++ src/modules/user/UserDetailsPage/index.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 const UserDetailsPage = () => {
     const { t } = useTranslation();
+    const user = useSelector(state => state.user);
+    useEffect(() => {
+        console.log(user);
+    }, []);
     return(
         <div className="flex bg-backgroundGray w-full h-full">
             <p>{t('userDetailsPage')}</p>
