8ecf2aa9b9f3ecf7c23747fa62c3c4e9602f7cc3
Nitin Mishra <nitin@amplework.com>
2025-01-17 18:03:12 +0530

    env added

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { getCookie } from '../utils';
 
-const BASE_URL = 'http://127.0.0.1:8000';
+// const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 let csrftoken = getCookie('csrftoken');
 
 const apiConfig = {
